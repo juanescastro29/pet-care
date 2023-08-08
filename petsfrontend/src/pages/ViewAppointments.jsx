73d60d4f8e1c6b0c10076cce73ba80d25219abf5
Juan Esteban Castro Molano <juanescastro29@gmail.com>
2023-08-08 14:19:40 -0500

    add loading views for view pets and view appointments

--- petsfrontend/src/pages/ViewAppointments.jsx
+++ petsfrontend/src/pages/ViewAppointments.jsx
@@ -3,13 +3,16 @@ import Table from "../components/Table";
 import { Link } from "react-router-dom";
 
 const ViewAppointments = () => {
-  const [appointments, setAppointments] = useState([]);
+  const [appointments, setAppointments] = useState({});
   const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     async function fetchAppointments() {
+      setLoading(true);
       const response = await fetch("http://localhost:4000/appointment");
       const data = await response.json();
+      setLoading(false);
       setAppointments(data.slice(10 * (page - 1), 10 * page));
     }
     fetchAppointments();
@@ -66,41 +69,53 @@ const ViewAppointments = () => {
               </div>
             </div>
           ) : (
-            <div className="hero min-h-screen" data-theme="cupcake">
-              <div className="hero-content text-center">
-                <div className="max-w-md">
-                  <h1 className="text-3xl font-bold py-10">
-                    No more appointments to show
-                  </h1>
-                  <div
-                    className="join w-full p-5 content-center justify-center"
-                    data-theme="cupcake"
-                  >
-                    {page > 1 ? (
-                      <button
-                        className="join-item btn"
-                        onClick={() => setPage(page - 1)}
-                      >
-                        «
-                      </button>
-                    ) : (
-                      <button className="join-item btn">«</button>
-                    )}
-                    <button className="join-item btn">Page {page}</button>
-                    {page >= 1 && appointments.length > 0 ? (
-                      <button
-                        className="join-item btn"
-                        onClick={() => setPage(page + 1)}
-                      >
-                        »
-                      </button>
-                    ) : (
-                      <button className="join-item btn">»</button>
-                    )}
+            <>
+              {loading === true ? (
+                <div className="hero min-h-screen" data-theme="cupcake">
+                  <div className="hero-content text-center">
+                    <div className="max-w-md">
+                      <span className="loading loading-spinner loading-lg"></span>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </div>
+              ) : (
+                <div className="hero min-h-screen" data-theme="cupcake">
+                  <div className="hero-content text-center">
+                    <div className="max-w-md">
+                      <h1 className="text-3xl font-bold py-10">
+                        No more pets to show
+                      </h1>
+                      <div
+                        className="join w-full p-5 content-center justify-center"
+                        data-theme="cupcake"
+                      >
+                        {page > 1 ? (
+                          <button
+                            className="join-item btn"
+                            onClick={() => setPage(page - 1)}
+                          >
+                            «
+                          </button>
+                        ) : (
+                          <button className="join-item btn">«</button>
+                        )}
+                        <button className="join-item btn">Page {page}</button>
+                        {page >= 1 && appointments.length > 0 ? (
+                          <button
+                            className="join-item btn"
+                            onClick={() => setPage(page + 1)}
+                          >
+                            »
+                          </button>
+                        ) : (
+                          <button className="join-item btn">»</button>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </>
           )}
         </>
       )}
--- petsfrontend/src/pages/ViewPets.jsx
+++ petsfrontend/src/pages/ViewPets.jsx
@@ -5,11 +5,14 @@ import Cards from "../components/Cards";
 const ViewPets = () => {
   const [pets, setPets] = useState({});
   const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     async function fetchPets() {
+      setLoading(true);
       const response = await fetch("http://localhost:4000/pet");
       const data = await response.json();
+      setLoading(false);
       setPets(data.slice(9 * (page - 1), 9 * page));
     }
     fetchPets();
@@ -19,7 +22,7 @@ const ViewPets = () => {
     <>
       {pets.length > 0 ? (
         <div className="min-h-screen" data-theme="cupcake">
-          <Cards pets={pets}/>
+          <Cards pets={pets} />
           <div
             className="join w-full p-5 content-center justify-center"
             data-theme="cupcake"
@@ -63,41 +66,53 @@ const ViewPets = () => {
               </div>
             </div>
           ) : (
-            <div className="hero min-h-screen" data-theme="cupcake">
-              <div className="hero-content text-center">
-                <div className="max-w-md">
-                  <h1 className="text-3xl font-bold py-10">
-                    No more pets to show
-                  </h1>
-                  <div
-                    className="join w-full p-5 content-center justify-center"
-                    data-theme="cupcake"
-                  >
-                    {page > 1 ? (
-                      <button
-                        className="join-item btn"
-                        onClick={() => setPage(page - 1)}
-                      >
-                        «
-                      </button>
-                    ) : (
-                      <button className="join-item btn">«</button>
-                    )}
-                    <button className="join-item btn">Page {page}</button>
-                    {page >= 1 && pets.length > 0 ? (
-                      <button
-                        className="join-item btn"
-                        onClick={() => setPage(page + 1)}
-                      >
-                        »
-                      </button>
-                    ) : (
-                      <button className="join-item btn">»</button>
-                    )}
+            <>
+              {loading === true ? (
+                <div className="hero min-h-screen" data-theme="cupcake">
+                  <div className="hero-content text-center">
+                    <div className="max-w-md">
+                      <span className="loading loading-spinner loading-lg"></span>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </div>
+              ) : (
+                <div className="hero min-h-screen" data-theme="cupcake">
+                  <div className="hero-content text-center">
+                    <div className="max-w-md">
+                      <h1 className="text-3xl font-bold py-10">
+                        No more pets to show
+                      </h1>
+                      <div
+                        className="join w-full p-5 content-center justify-center"
+                        data-theme="cupcake"
+                      >
+                        {page > 1 ? (
+                          <button
+                            className="join-item btn"
+                            onClick={() => setPage(page - 1)}
+                          >
+                            «
+                          </button>
+                        ) : (
+                          <button className="join-item btn">«</button>
+                        )}
+                        <button className="join-item btn">Page {page}</button>
+                        {page >= 1 && pets.length > 0 ? (
+                          <button
+                            className="join-item btn"
+                            onClick={() => setPage(page + 1)}
+                          >
+                            »
+                          </button>
+                        ) : (
+                          <button className="join-item btn">»</button>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </>
           )}
         </>
       )}
